7b7a9d7d4ffde61f12354a6dac962bb0147d1b76
Artem M <artemmarin91@mail.ru>
2023-06-15 08:24:57 +0500

    upd Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,18 +5,20 @@ type Props = {
   onChange: (id: string) => void;
   selectedId: string;
   graphList: number[];
-}
+};
 
 const Header = ({ onChange, selectedId, graphList }: Props) => {
   const handleChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
-    onChange(e.target.value)
+    onChange(e.target.value);
   };
 
   return (
     <div className={s.header}>
       <span className={s.h1}>Graph</span>
       <select role="combobox" className={s.select} onChange={handleChange} value={selectedId}>
-        <option role="placeholder" value="" disabled>Выберите граф</option>
+        <option role="placeholder" value="" disabled>
+          Выберите граф
+        </option>
         {graphList.map((graphId) => (
           <option className={s.option} key={graphId} value={graphId}>
             {graphId}

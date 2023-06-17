9b53be05dbdd17deeef70f9378841794d26ea40d
Artem M <artemmarin91@mail.ru>
2023-06-17 23:12:37 +0500

    updated Graph component

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,31 +1,37 @@
-import React, { useState } from 'react';
-import GraphType from '../../types/graph';
-import sortNodes from '../../helpers/sortNodes';
+import React, { useEffect, useState } from 'react';
+import GraphType, { GraphEdge } from '../../types/graph';
 import Edge from '../Edge/Edge';
 import s from './Graph.module.css';
+import DragnDrop from '../DragnDrop/DragnDrop';
+import useSortGraph from '../../helpers/useSortGraph';
 
 type Props = {
   selectedGraph: GraphType;
 };
 
 const Graph = ({ selectedGraph }: Props) => {
-  const columns = sortNodes(selectedGraph);
+  const [graphColumns, changeGraphColumns] = useSortGraph(selectedGraph);
+  const [edges, setEdges] = useState<GraphEdge[]>(selectedGraph.edges);
+
+  useEffect(() => {
+    setEdges(selectedGraph.edges);
+  }, [selectedGraph]);
 
   return (
     <div className={s.container}>
       <div className={s.columns}>
-        {columns.map((col, i) => (
-          <div key={i} className={s.column}>
-            {col.map((item) => (
-              <div key={item.id} className={s.nodeItem} id={`${item.id}`}>
-                {item.name}
-              </div>
-            ))}
-          </div>
+        {graphColumns.map((col, i) => (
+          <DragnDrop
+            key={i}
+            draggableList={col}
+            droppableIndex={i}
+            graphColumns={graphColumns}
+            changeGraphColumns={changeGraphColumns}
+          />
         ))}
       </div>
-      {selectedGraph.edges.map((edge, i) => (
-        <Edge key={i} edge={edge} />
+      {edges.map((edge, i) => (
+        <Edge key={i} edge={edge} columns={graphColumns} />
       ))}
     </div>
   );

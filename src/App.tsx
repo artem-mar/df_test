aa0f0654148890bd655405f2671c68e904966c06
Artem M <artemmarin91@mail.ru>
2023-06-17 23:13:29 +0500

    updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Graph from "./components/Graph/Graph";
-import { fetchGraphsList, fetchGraph } from "./api";
-import GraphType from './types/graph';
+import { fetchGraphsList } from "./api";
 import s from "./App.module.css";
+import useGraph from "./helpers/useGrahp";
 
 function App() {
   const [graphId, setGraphId] = useState('');
   const [graphList, setGraphList] = useState<number[]>([]);
-  const [graph, setGraph] = useState<GraphType>({ nodes: [], edges: [] });
+
+  const graph = useGraph(graphId);
 
   useEffect(() => {
     fetchGraphsList().then((list) => setGraphList(list));
   }, []);
-  useEffect(() => {
-    graphId && fetchGraph(graphId).then((graph) => setGraph(graph));
-  }, [graphId]);
 
   return (
     <div className={s.page}>
       <Header onChange={setGraphId} selectedId={graphId} graphList={graphList} />
       <main className={s.main}>
-        <Graph selectedGraph={graph} />
+        {!!graphId && <Graph selectedGraph={graph} />}
       </main>
     </div>
   );
